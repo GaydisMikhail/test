933cbb338b7123c953c68598c72b0c82d11af48c
GaydisMikhail <gaydisMikhail@gmail.com>
2016-11-01 18:04:53 +0300

    Update connect.php

--- bd/connect.php
+++ bd/connect.php
@@ -1,7 +1,4 @@
 <?
-	session_start();
-	$_pdo = new PDO('mysql:host=localhost;dbname=3390017;','root','');
-
 	class connect {
 		public static $db = "mysql:host=localhost;dbname=3390017;";
 		public static $us = "root";
